e1118a0b0def42d1e2b2b2f169e0042a1717f6f7
Mohammad Reza Hashempour <mohammadhprp@gmail.com>
2026-09-24 18:01:19 +0330

    chore(release): v0.1.1

--- manifest.config.ts
+++ manifest.config.ts
@@ -12,7 +12,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Excalidraw Sync",
-  version: "0.1.0",
+  version: "0.1.1",
   description: "Save and sync Excalidraw boards to a GitHub repository.",
   minimum_chrome_version: "120",
   permissions: ["storage"],
